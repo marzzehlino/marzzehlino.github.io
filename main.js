6d991400cb0eb75baf96839668e1094076589b82
marzzehlino <osp.marzzeh@gmail.com>
2022-06-23 12:10:49 +0200

    First GIT

---
+++ main.js
@@ -0,0 +1,15 @@
+$('#Collapse1').on('shown.bs.collapse', function () {
+    this.scrollIntoView();
+});
+
+$('#Collapse2').on('shown.bs.collapse', function () {
+    this.scrollIntoView();
+});
+
+$('#Collapse3').on('shown.bs.collapse', function () {
+    this.scrollIntoView();
+});
+
+$('#Collapse4').on('shown.bs.collapse', function () {
+    this.scrollIntoView();
+});
